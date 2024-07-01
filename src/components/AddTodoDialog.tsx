c4ffdd17b0345bcc27bab6ee1fee4c7c67c69f29
Haugen <tobiashaugen@gmail.com>
2024-07-01 19:33:41 +0200

    dialog instead of alert dialog

--- src/components/AddTodoDialog.tsx
+++ src/components/AddTodoDialog.tsx
@@ -1,38 +1,31 @@
 import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import { X } from 'react-feather';
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
 import { Button } from './ui/button';
 import AddTodoForm from './AddTodoForm';
 
 export default async function AddTodoDialog() {
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="outline">Add Todo</Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <div className="flex justify-between items-center mb-2">
-            <h2 className="text-lg font-semibold mb-0">Let's add a Todo</h2>
-            <AlertDialogCancel className="rounded-full p-0 w-10 h-10">
-              <X />
-            </AlertDialogCancel>
-          </div>
-          <AlertDialogDescription>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Let's add a Todo</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
             Name your new Todo below, and click "Add Todo". It'll automatically
             be included in your active Todos.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
+          </DialogDescription>
+        </DialogHeader>
         <AddTodoForm />
-        <AlertDialogFooter></AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+      </DialogContent>
+    </Dialog>
   );
 }
